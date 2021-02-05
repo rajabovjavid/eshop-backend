4fd74db1e208e4113fb22241641f2b4c0ffddc4c
rajabovjavid <jvdrjbv@gmail.com>
2021-02-05 21:35:15 +0400

    Category and Product routes are completed

--- routers/category.js
+++ routers/category.js
@@ -1,77 +1,79 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
-const { Category } = require("../models/category");
+const { Category } = require("../models/Category");
 
 router.get("/", async (req, res) => {
-  const categoryList = await Category.find();
+  const categories = await Category.find();
 
-  if (!categoryList) {
+  if (!categories) {
     res.status(500).json({ success: false });
   }
-  res.send(categoryList);
+  res.json({ categories, success: true });
 });
 
-router.get("/:id", async (req, res)=>{
-  const category = await Category.findById(req.params.id)
-  if(!category){
-    res.status(500).json({message: "The category was not found"}) 
+router.get("/category", async (req, res) => {
+  if (!mongoose.isValidObjectId(req.query.id)) {
+    res.status(400).json({ success: false, message: "Invalid Category Id" });
   }
 
-  res.status(200).send(category)
+  const category = await Category.findById(req.query.id);
 
-  /* Category.findById(req.params.id).then(category=>{
-    if (category) {
-      res.status(200).send(category)
-    } else {
-      res.status(500).json({message: "The category was not found"})
-    }
-  }).catch(err=>{
-    res.status(400).json({success: false, error: err})
-  }) */
-})
+  if (!category) {
+    res
+      .status(404)
+      .json({ message: "The category was not found", success: false });
+  }
+  res.status(200).send({ category, success: true });
+});
 
-router.post("/", async (req, res)=>{
-  let category = new Category(req.body)
-
-  category = await category.save()
-
-  if(!category){
-    return res.status(404).send("category is not created")
+router.post("/", async (req, res) => {
+  let category = new Category(req.body);
+  try {
+    category = await category.save();
+  } catch (error) {
+    res.status(400).json({ error, success: false });
   }
 
-  res.send(category)
-})
+  if (!category) {
+    res
+      .status(404)
+      .json({ message: "category is not created", success: false });
+  }
+  res.json({ category, success: true });
+});
 
-router.put("/:id", async (req, res)=>{
-  Category.findByIdAndUpdate(req.params.id, req.body, {new: true}).then(cat=>{
-    if (cat) {
-      res.status(200).send(cat)
-    } else {
-      res.status(500).json({message: "category was not updated"})
-    }
-  }).catch(err=>{
-    res.status(400).json({error: err})
-  })
-})
-
-router.delete("/:id", async (req, res)=>{
-  /* const category = await Category.findByIdAndDelete(req.params.id)
-
-  if(!category){
-    return res.status(404).json({success: false, message: "category is not deleted"})
+router.put("/category", async (req, res) => {
+  if (!mongoose.isValidObjectId(req.query.id)) {
+    res.status(400).json({ success: false, message: "Invalid Category Id" });
   }
 
-  return res.status(200).json({success: true, message: "category is deleted"}) */
+  const category = await Category.findByIdAndUpdate(req.query.id, req.body, {
+    new: true,
+  });
 
-  Category.findByIdAndDelete(req.params.id).then(category=>{
-    if (category) {
-      return res.status(200).json({success: true, message: "category is deleted"})
-    } else {
-      return res.status(404).json({success: false, message: "category is not deleted"})
-    }
-  }).catch(err=>{
-    res.status(400).json({success: false, error: err})
-  })
-})
+  if (!category) {
+    res
+      .status(404)
+      .json({ message: "category was not updated", success: false });
+  }
+  res.status(200).json({ category, success: true });
+});
+
+router.delete("/", async (req, res) => {
+  if (!mongoose.isValidObjectId(req.body.id)) {
+    res.status(400).json({ success: false, message: "Invalid Category Id" });
+  }
+  const category = await Category.findByIdAndDelete(req.body.id);
+
+  if (!category) {
+    return res
+      .status(404)
+      .json({ success: false, message: "category is not deleted" });
+  }
+  return res
+    .status(200)
+    .json({ success: true, message: "category is deleted" });
+});
 
 module.exports = router;
--- routers/product.js
+++ routers/product.js
@@ -1,31 +1,141 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { Category } = require("../models/Category");
 const router = express.Router();
-
-const {Product} = require("../models/Product");
+const { Product } = require("../models/Product");
 
 router.get("/", async (req, res) => {
-  const productList = await Product.find();
-  if (!productList) {
+  if (req.query.category) {
+    //checking category id
+    if (!mongoose.isValidObjectId(req.query.category)) {
+      res.status(400).json({ success: false, message: "Invalid Category Id" });
+    }
+
+    // checking category
+    const category = await Category.findById(req.query.category);
+    if (!category) {
+      res
+        .status(404)
+        .json({ message: "Category was not found", success: false });
+    }
+  }
+
+  const categoryFilter = req.query.category
+    ? { category: req.query.category }
+    : {};
+
+  const products = await Product.find(categoryFilter).populate("category");
+  if (!products) {
     res.status(500).json({
       success: false,
     });
   }
-  res.send(productList);
+  res.json({ products, success: true });
 });
 
-router.post("/", (req, res) => {
-  const product = new Product(req.body);
-  product
-    .save()
-    .then((newProduct) => {
-      res.status(201).json(newProduct);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-        success: false,
-      });
+router.get("/product", async (req, res) => {
+  if (!mongoose.isValidObjectId(req.query.id)) {
+    res.status(400).json({ success: false, message: "Invalid Product Id" });
+  }
+
+  const product = await Product.findById(req.query.id).populate("category"); // select("name")
+  if (!product) {
+    res
+      .status(404)
+      .json({ message: "The product was not found", success: false });
+  }
+  res.status(200).json({ product, success: true });
+});
+
+router.get("/count", async (req, res) => {
+  const count = await Product.countDocuments();
+  if (!count) {
+    res.status(500).json({
+      success: false,
     });
+  }
+  res.json({ count, success: true });
+});
+
+router.get("/featured", async (req, res) => {
+  const limit = req.query.limit ? req.query.limit : 0;
+  const products = await Product.find({ isFeatured: true }).limit(
+    parseInt(req.query.limit)
+  );
+  if (!products) {
+    res.status(500).json({
+      success: false,
+    });
+  }
+  res.json({ products, success: true });
+});
+
+router.post("/", async (req, res) => {
+  //checking category id
+  if (!mongoose.isValidObjectId(req.body.category)) {
+    res.status(400).json({ success: false, message: "Invalid Category Id" });
+  }
+
+  // checking category
+  const category = await Category.findById(req.body.category);
+  if (!category) {
+    res.status(404).json({ message: "Category was not found", success: false });
+  }
+
+  let product = new Product(req.body);
+  try {
+    product = await product.save();
+  } catch (error) {
+    res.status(400).json({ message: error.message, success: false });
+  }
+
+  if (!product) {
+    res.json({ message: "Product was not created", success: false });
+  }
+  res.json({ product, success: true });
+});
+
+router.put("/product", async (req, res) => {
+  //checking category id
+  if (!mongoose.isValidObjectId(req.body.category)) {
+    res.status(400).json({ success: false, message: "Invalid Category Id" });
+  }
+
+  // checking category
+  const category = await Category.findById(req.body.category);
+  if (!category) {
+    res.status(404).json({ message: "Category was not found", success: false });
+  }
+
+  //checking product id
+  if (!mongoose.isValidObjectId(req.query.id)) {
+    res.status(400).json({ success: false, message: "Invalid Product Id" });
+  }
+
+  const product = await Product.findByIdAndUpdate(req.query.id, req.body, {
+    new: true,
+  });
+
+  if (!product) {
+    res
+      .status(404)
+      .json({ message: "product was not updated", success: false });
+  }
+  res.status(200).json({ product, success: true });
+});
+
+router.delete("/", async (req, res) => {
+  if (!mongoose.isValidObjectId(req.body.id)) {
+    res.status(400).json({ success: false, message: "Invalid Product Id" });
+  }
+  const product = await Product.findByIdAndDelete(req.body.id);
+
+  if (!product) {
+    return res
+      .status(404)
+      .json({ success: false, message: "product is not deleted" });
+  }
+  return res.status(200).json({ success: true, message: "product is deleted" });
 });
 
 module.exports = router;
